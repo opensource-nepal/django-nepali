960ec09f20e0527e74bd0e0b85a90d4da86ad875
Ajesh Sen Thapa <aj3sshh@gmail.com>
2023-05-22 22:05:42 +0545

    Migrated templatetags from py-nepali project

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # django-nepali
+
 A django package on top of 'nepali' a python package which supports nepali date time, time conversion, etc on django projects.
+
+## Requirements
+
+    django
+    nepali >= 1.0.0
+
+## Installation
+
+    pip install django-nepali
+
+On `settings.py`, add `'django_nepali'` to your `INSTALLED_APPS`.
+
+```python
+INSTALLED_APPS = [
+    ...
+    'django_nepali',
+    ...
+]
+```
+
+## Django Templates
+
+### nepalidatetime
+
+In your Template
+
+```python
+{% load nepalidatetime %}
+```
+
+#### nepalinow
+
+`nepalinow` renders the current Nepali date and time in 'en-US' locale (English).
+
+```python
+{% nepalinow %}
+```
+
+```python
+{% nepalinow '%Y-%m-%d' %}
+```
+
+#### nepalinow_ne
+
+`nepalinow_ne` renders the current Nepali date and time in 'ne' locale (Nepali).
+
+```python
+{% nepalinow_ne %}
+```
+
+#### nepalidate
+
+`nepalidate` renders the datetime object into nepali datetime format in 'en-US' locale (English).
+
+```python
+{{ datetime_obj|nepalidate:"%Y-%m-%d" }}
+```
+
+#### nepalidate_ne
+
+`nepalidate_ne` renders the datetime object into nepali datetime format in 'ne' locale (Nepali).
+
+```python
+{{ datetime_obj|nepalidate_ne:"%Y-%m-%d" }}
+```
+
+#### nepalihumanize
+
+`nepalihumanize` renders the datetime object to a human readable form for 'ne' locale (Nepali)
+
+```python
+{{ datetime_obj|nepalihumanize }}
+```
+
+### nepalinumber
+
+In your Template
+
+```python
+{% load nepalinumber %}
+```
+
+`nepalinumber` renders the english number into nepali format (devanagari)
+
+```python
+{{ forloop.counter|nepalinumber }}
+
+{{ 150|nepalinumber }}
+```
---
+++ setup.py
@@ -0,0 +1,65 @@
+"""
+setup file for django-nepali package
+
+- Building a package
+
+    pip install build
+    python -m build
+
+
+- Publishing a package
+You must have twine installed in your system. `pip install twine`
+
+    python setup.py sdist bdist_wheel
+    twine upload dist/*
+
+"""
+import os
+import sys
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+if sys.argv[-1] == "publish":
+    if os.system("pip3 freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("rm -rf dist")
+    os.system("python3 setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    sys.exit()
+
+
+setuptools.setup(
+    name="django-nepali",
+    version="0.1.0",
+    author="opensource-nepal",
+    author_email="aj3sshh@gmail.com, sugatbajracharya49@gmail.com",
+    description="A django package on top of 'nepali' a python package which supports nepali date time, time conversion, etc on django projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "django",
+        "nepali date conversion",
+        "convert date",
+        "nepali date time",
+        "python convert date",
+        "parse nepali date time",
+    ],
+    url="https://github.com/opensource-nepal/django-nepali",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    install_requires=[
+        'django',
+        'nepali>=1.0.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL License",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+    ],
+)
